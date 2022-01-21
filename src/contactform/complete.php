2b5a81f3752d763507815c4af96df7e091b814c2
satomi212 <matarou0212@gmail.com>
2022-01-21 21:13:37 +0900

    complete.phpの修正

--- src/contactform/complete.php
+++ src/contactform/complete.php
@@ -5,7 +5,6 @@ $title = filter_input(INPUT_POST, 'title');
 $email = filter_input(INPUT_POST, 'email');
 $content = filter_input(INPUT_POST, 'content');
 
-
 // DB接続
 $dsn = 'mysql:dbname=contact_form;host=mysql;charset=utf8';
 $user = 'root';
@@ -13,7 +12,8 @@ $password = 'password';
 $dbh = new PDO($dsn, $user, $password);
 
 // SQL
-$sql = "INSERT INTO contacts (title, email, content) VALUES (:title, :email, :content)";
+$sql =
+    'INSERT INTO contacts (title, email, content) VALUES (:title, :email, :content)';
 
 // prepareでSQLを確定
 $stmt = $dbh->prepare($sql);
@@ -26,24 +26,23 @@ $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 // executeでSQLを実行
 $stmt->execute();
 
-
 // 送信方法バリデーション
 $errors = [];
-if ($_SERVER["REQUEST_METHOD"] != "POST") {
-    $errors[] = "post送信になっていません";
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    $errors[] = 'post送信になっていません';
 }
 
 // 入力内容バリデーション
 if (empty($title)) {
-    $errors[] = "「タイトル」を入力してください。";
+    $errors[] = '「タイトル」を入力してください。';
 }
 
 if (empty($email)) {
-    $errors[] = "「Email」を入力してください。";
+    $errors[] = '「Email」を入力してください。';
 }
 
 if (empty($content)) {
-    $errors[] = "「内容」を入力してください。";
+    $errors[] = '「内容」を入力してください。';
 }
 
 // 中身
@@ -62,10 +61,6 @@ if (empty($errors)) {
     <p>送信画面へ</p>
     </a>';
 }
-
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -79,17 +74,17 @@ if (empty($errors)) {
 
 <body>
     <div class="container">
-        <?php if (!empty($errors)) : ?>
-            <?php foreach ($errors as $error) : ?>
+        <?php if (!empty($errors)): ?>
+            <?php foreach ($errors as $error): ?>
                 <p><?php echo $error . "\n"; ?></p>
             <?php endforeach; ?>
             <?php echo $links; ?>
         <?php endif; ?>
 
-        <?php if (empty($errors)) : ?>
+        <?php if (empty($errors)): ?>
             <?php
-                echo '<h2>' . $message . '</h2>';
-                echo $links;
+            echo '<h2>' . $message . '</h2>';
+            echo $links;
             ?>
         <?php endif; ?>
     </div>
