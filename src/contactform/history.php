f7facba1384c373a1fdc6c3c7aaffd8109c10aab
satomi212 <matarou0212@gmail.com>
2022-01-21 21:13:56 +0900

    history.phpの修正

--- src/contactform/history.php
+++ src/contactform/history.php
@@ -1,6 +1,5 @@
 
-<?php
-try {
+<?php try {
     // DB接続
     $dsn = 'mysql:dbname=contact_form;host=mysql;charset=utf8';
     $user = 'root';
@@ -8,7 +7,7 @@ try {
     $dbh = new PDO($dsn, $user, $password);
 
     // SQL
-    $sql = "SELECT * FROM contacts";
+    $sql = 'SELECT * FROM contacts';
 
     // prepareでSQLを確定
     $stmt = $dbh->prepare($sql);
@@ -18,12 +17,10 @@ try {
 
     // 結果の取得
     $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 } catch (PDOException $e) {
     echo 'データベースに接続できませんでした。' . $e->getMessage();
     exit();
-}
-?>
+} ?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -38,9 +35,9 @@ try {
     <div class="container">
         <h2>送信履歴</h2>
 
-        <?php foreach ($contacts as $contact) : ?>
-            <h3><?php echo $contact['title'] ?></h3>
-            <p><?php echo $contact['content'] ?></p>
+        <?php foreach ($contacts as $contact): ?>
+            <h3><?php echo $contact['title']; ?></h3>
+            <p><?php echo $contact['content']; ?></p>
         <?php endforeach; ?>
 
         <a href="index.php">戻る</a>
